a596c37c3156e64dab8b72fbbf256cabda6dc29c
gargsona7130 <gargsonal7130@gmail.com>
2025-01-08 13:11:14 +0530

    Components created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
 
-const App = () => {
-  return (
-    <div>
-      
-    </div>
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './components';
+
+/* 
+Initially importing all here like this
+but now imported it like above.
+
+import Navbar from './components/Navbar';
+import VideoDetail from './components/VideoDetail';
+import ChannelDetail from './components/ChannelDetail';
+import {Feed} from '.@mui/icons-material';
+import SearchFeed from './components/SearchFeed';
+*/
+
+const App = () => (
+    <BrowserRouter>
+    <Box sx={{ backgroundColor: '#000' }} >
+       <Navbar />
+       <Routes>
+        <Route path="/" exact element = {<Feed />} />
+        <Route path="video/:id" element={<VideoDetail />} />
+        <Route path="channel/:id" element={<ChannelDetail />} />
+        <Route path="search/:searchTerm" element={<SearchFeed />} />
+
+        </Routes> 
+    </Box>
+    </BrowserRouter>
   )
-}
+
 
 export default App
 
